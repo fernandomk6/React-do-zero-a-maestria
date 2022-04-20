8c71f4bfe658b31878c176dfd49e9c342ba71248
fernando henrique <fernandomk6@gmail.com>
2022-04-20 12:32:10 -0300

    props

--- 3-avancado/myapp/src/App.js
+++ 3-avancado/myapp/src/App.js
@@ -5,8 +5,11 @@ import Desert from './components/Desert';
 import ManageData from './components/ManageData';
 import ListRender from './components/ListRender';
 import ConditionalRender from './components/ConditionalRender';
+import ShowUserName from './components/ShowUserName';
 
 function App() {
+  const userAge = 23;
+  
   return (
     <div className="App">
       <h1>Avançado em React</h1>
@@ -15,6 +18,7 @@ function App() {
       <ManageData />
       <ListRender />
       <ConditionalRender />
+      <ShowUserName name="fernando" age={userAge} />
     </div>
   );
 }
---
+++ 3-avancado/myapp/src/components/ShowUserName.js
@@ -0,0 +1,12 @@
+const ShowUserName = ({name, age}) => {
+
+  return (
+    <div>
+      <h4>ShowUserName</h4>
+      <p>O nome do usuario é: {name}
+      e ele tem {age} anos.</p>
+    </div>
+  );
+};
+
+export default ShowUserName;
