7c8e32b5e3f24781e9f7434d9062d2db499144b4
fernando henrique <fernandomk6@gmail.com>
2022-04-25 17:03:12 -0300

    estruturando componente game e css da tela do jogo

--- 6-projeto-secret-word/secretword/src/components/Game.js
+++ 6-projeto-secret-word/secretword/src/components/Game.js
@@ -2,9 +2,32 @@ import "./Game.css";
 
 const Game = ({verifyLetter}) => {
   return (
-    <div>
-      <h2>Game</h2>
-      <button onClick={verifyLetter}>Finalizar o jogo</button>
+    <div className="game">
+      <p className="points">
+        <span>Pontuação: 000</span>
+      </p>
+      <h1>Adivinhe a palavra</h1>
+      <h3 className="tip">
+        Dica sobre a palavra <span>dica...</span>
+      </h3>
+      <div className="wordContainer">
+        <span className="letter">A</span>
+        <span className="blankSquare"></span>
+      </div>
+      <div className="letterContainer">
+        <p>Tente adivinhar uma letra da palavra</p>
+        <form>
+          <input type="text" name="letter" maxLenght="1" required="true"/>
+          <button>Jogar</button>
+        </form>
+      </div>
+      <div className="wrongLettersContainer">
+        <p>
+          Letras já utilizadas: 
+          <span>a, </span>
+          <span>b, </span>
+        </p>
+      </div>
     </div>
   );
 };
