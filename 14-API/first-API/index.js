0d406050f39cfefd5460355c8ffc0422500dbaec
fernando henrique <fernandomk6@gmail.com>
2022-06-03 23:06:50 -0300

    criado primeira api com express. Maravilhoso!!!!

---
+++ 14-API/first-API/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+);
+  
+app.use(express.json());
+
+// endpoints
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'first router success'
+  });
+});
+
+app.post('/createproduct', (req, res) => {
+  const name = req.body.name;
+  const price = req.body.price;
+
+  if (!name) {
+    res.status(200).json({
+      maessage: 'um dos parametros é inválido'
+    });
+    return;
+  }
+
+  res.status(201).json({
+    message: `produto ${name}, criado com sucesso ${price}`
+  });
+  return;
+});
+
+app.listen(3000);
